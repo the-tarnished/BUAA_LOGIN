ce8866a711086528476883ca75317bbd9ce50060
岳诗扬 <20373057@buaa.edu.cn>
2022-05-08 12:13:08 +0800

    initial

---
+++ README.md
@@ -0,0 +1,41 @@
+﻿# Readme
+## 文件介绍
+`connect_with_page`文件在运行的时候会弹出浏览器窗口
+`connect_without_page`文件在运行的时候不会弹出浏览器窗口
+请自行选择合适的文件
+## 前置条件
+### python运行环境
+自行下载python运行环境
+### 下载第三方库
+
+```
+pip install -r .\requirements.txt
+```
+
+### 下载浏览器驱动(支持Edge,Chrome,Firefox)
+[Edge](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)
+[Chrome](http://chromedriver.storage.googleapis.com/index.html)
+[Firefox](https://github.com/mozilla/geckodriver/releases)
+下载时记得核对自己浏览器和浏览器驱动版本
+建议选择Chrome,代码在Chrome爬取时使用了CDP,能一定程度上反屏蔽
+
+### 添加系统路径
+在代码运行时需要寻找对应驱动,因此记得将内核的地址放在环境变量中
+这里特别推荐将内核放入Python的Scripts文件夹中,这样就可以不用在环境变量中特别增加关于内核的变量,效果如下:
+![图 1](../images/f219870bcdc39f63b8922ab54183bb61f33b092ad463181ccaf1157c589fb19c.png)  
+
+
+## 修改Config
+进入代码,Config区域中有
+```python
+###CONFIG
+username = ""
+password = ""
+typeOfDriver = "Firefox" ##Firefox Edge Chrome
+url = "https://gw.buaa.edu.cn/srun_portal_pc?ac_id=1&theme=buaa"
+### END CONFIG
+```
+修改对应的`username`,`password`即可
+
+## 结束语
+代码编写仓促,如有bug,请联系作者
---
+++ connect_without_page.py
@@ -0,0 +1,61 @@
+﻿###CONFIG
+username = ""
+password = ""
+typeOfDriver = "Firefox" ##Firefox Edge Chrome
+url = "https://gw.buaa.edu.cn/srun_portal_pc?ac_id=1&theme=buaa"
+### END CONFIG
+
+
+
+### import
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver import EdgeOptions
+from selenium.webdriver import FirefoxOptions
+import logging
+import time
+
+browser = None
+logging.basicConfig(level=logging.INFO,format = '%(asctime)s - %(levelname)s: %(message)s')
+
+def initBrowser() :
+    global browser
+    if typeOfDriver == "Chrome":
+        option = ChromeOptions()
+        option.add_experimental_option('excludeSwitches',['enable-automation'])
+        option.add_experimental_option('useAutomationExtension',False)
+        option.add_argument('--headless')
+        browser = webdriver.Chrome(options = option)
+        browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',{'source':'Object.defineProperty(navigator,"webdriver",{get:() => undefined})'})
+    elif typeOfDriver == "Edge":
+        option = EdgeOptions();
+        option.add_argument('--headless')
+        browser = webdriver.Edge(options=option)
+    elif typeOfDriver == "Firefox":
+        option = FirefoxOptions();
+        option.add_argument('--headless')
+        browser = webdriver.Firefox(options=option)
+    else :
+        logging.info("Wrong Driver Type")
+
+def connect() :
+    browser.get(url)
+    try:
+        usernameInput = browser.find_element(By.ID,"username")
+        logging.info("Input username")
+        usernameInput.send_keys(username)
+        time.sleep(1)
+        passwordInput = browser.find_element(By.ID,"password")
+        logging.info("Input password")
+        passwordInput.send_keys(password)
+        time.sleep(1)
+        button = browser.find_element(By.ID,"login")
+        logging.info("Login")
+        button.click()
+    except:
+        logging.info("Already Login")
+    
+if __name__ == "__main__":
+    initBrowser()
+    connect()
